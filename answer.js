c54aa476c3f33b6acca7dc10974cb7c6e1f07573
zeyadetman <zeyadetman@gmail.com>
2018-02-11 22:56:29 +0200

    last updates

--- answer.js
+++ answer.js
@@ -24,40 +24,46 @@ var userInput = "";
             "logo": "1p.jpg"
         }, ];
 
-        function start() {
-            document.getElementsByClassName("logo")[0].setAttribute("src", logos[index].logo);
-            document.getElementById("user-try").style.color = "#fc3";
-            document.getElementById("user-try").style.borderBottom = "2px solid #fc3";
-            document.getElementById("user-try").value = "";
-            //document.getElementById("user-try").innerHTML = ""; //.toLowerCase();     
-        }
+function start(index,score) {
+    document.getElementsByClassName("logo")[0].setAttribute("src", logos[index].logo);
+    document.getElementById("user-try").style.color = "#fc3";
+    document.getElementById("user-try").style.borderBottom = "2px solid #fc3";
+    document.getElementById("user-try").value = ""; 
+    document.getElementsByClassName("scor-count")[0].innerHTML = score.toString();
+}
 
-        function check() {
-            userInput = document.getElementById("user-try").value.toLowerCase();
-            if (userInput == logos[index].brand) {
-                document.getElementById("user-try").style.color = "green";
-                document.getElementById("user-try").style.borderBottom = "2px solid green";
-                index++;
-                if (index == logos.length) {
-                    var x = confirm("Do you want to play again");
-                    if (x) {
-                        index = 0;
-                        score = 0;
-                        document.getElementsByClassName("scor-count")[0].innerHTML = "0";
-                        start();
-                    } else {
-                        alert(`Your score is ${score}`);
-                    }
+function check() {
+    userInput = document.getElementById("user-try").value.toLowerCase();
+    console.log(index);
+    if (userInput == logos[index].brand) {
+            document.getElementById("user-try").style.color = "green";
+            document.getElementById("user-try").style.borderBottom = "2px solid green";
+            index++;
+            if (index == logos.length) {
+                    alert("Wow! You solved all logos! ");
+                    gameOver(score);
+                    start(0,0);
                 }
                 index = (index) % logos.length;
                 // console.log(index);
                 score += 5;
                 document.getElementsByClassName("scor-count")[0].innerHTML = score.toString();
-                setTimeout(start, 2000);
+                setTimeout(start, 1000);
+                start(index,score);
             } else {
                 //wrong
                 document.getElementById("user-try").style.color = "red";
                 document.getElementById("user-try").style.borderBottom = "2px solid red";
+                alert("Ooh! it's gameOver! ");                
+                gameOver(score);
+                start(0,0);
             }
-        }
-        start();
+}
+start(index,score);
+
+function gameOver(pScore){
+    var player = prompt("write your name");
+    localStorage.setItem(player,pScore);
+    index=0;
+    score=0;
+}
